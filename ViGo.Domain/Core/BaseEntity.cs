916d293a1dbf788f59e298ab197f0c621d28848c
Tran Phong <n.tranphongse@gmail.com>
2023-05-08 18:11:59 +0700

    Domain Core classes

---
+++ ViGo.Domain/Core/BaseEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViGo.Domain.Core
+{
+    // Base Class for Entity
+    public abstract class BaseEntity
+    {
+    }
+
+    #region Interfaces
+    public interface ISoftDeletedEntity
+    {
+        bool IsDeleted { get; set; }
+    }
+
+    public interface ITrackingCreated
+    {
+        string CreatedBy { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+
+    public interface ITrackingUpdated
+    {
+        string UpdatedBy { get; set; }
+        DateTime UpdatedDate { get; set; }
+    }
+    #endregion
+}
---
+++ ViGo.Domain/Core/DomainCore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViGo.Domain.Core
+{
+    // Make sure all domain classes inherit the BaseEntity class and necessary interfaces
+}
