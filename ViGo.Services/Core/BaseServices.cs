c86645608d05f8e14a55caa84d3fae84bb10ae5b
Tran Phong <n.tranphongse@gmail.com>
2023-05-08 18:24:36 +0700

    Base Services created

---
+++ ViGo.Services/Core/BaseServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViGo.Domain.Core;
+using ViGo.Repository.Core;
+
+namespace ViGo.Services.Core
+{
+    public abstract class BaseServices<T> where T : BaseEntity
+    {
+        protected IUnitOfWork work;
+
+        public BaseServices(IUnitOfWork work)
+        {
+            this.work = work;
+        }
+    }
+}
