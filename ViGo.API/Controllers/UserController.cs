83409bd83552456cb2f9c61b3008ec4e719e2024
Tran Phong <n.tranphongse@gmail.com>
2023-05-14 22:14:47 +0700

    UserController - Example usage and test

---
+++ ViGo.API/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ViGo.Domain;
+using ViGo.Repository.Core;
+using ViGo.Services;
+using ViGo.Utilities.Extensions;
+
+namespace ViGo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserServices userServices;
+
+        public UserController(IUnitOfWork work)
+        {
+            userServices = new UserServices(work);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            try
+            {
+                IEnumerable<Domain.User> users =
+                    await userServices.GetUsersAsync();
+                return StatusCode(200, users);
+            } catch (ApplicationException ex)
+            {
+                return StatusCode(400, ex.GeneratorErrorMessage());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.GeneratorErrorMessage());
+            }
+        }
+    }
+}
