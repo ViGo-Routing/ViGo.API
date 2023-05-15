523d177d3b9cd5bae6b5187cd6da77480d9ef37c
Tran Phong <n.tranphongse@gmail.com>
2023-05-15 14:15:52 +0700

    User - Basic Login
    Login with JWT and basic Authorization

---
+++ ViGo.API/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ViGo.Domain;
+using ViGo.DTOs.Users;
+using ViGo.Repository.Core;
+using ViGo.Services;
+using ViGo.Utilities.Configuration;
+using ViGo.Utilities.Extensions;
+
+namespace ViGo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserServices userServices;
+
+        public UserController(IUnitOfWork work)
+        {
+            userServices = new UserServices(work);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            try
+            {
+                IEnumerable<Domain.User> users =
+                    await userServices.GetUsersAsync();
+                return StatusCode(200, users);
+            } catch (ApplicationException ex)
+            {
+                return StatusCode(400, ex.GeneratorErrorMessage());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.GeneratorErrorMessage());
+            }
+        }
+
+        [HttpPost("Login")]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginUserDto loginUser)
+        {
+            try
+            {
+                User user = await userServices.Login(
+                    loginUser.Email, loginUser.Password);
+
+                if (user == null)
+                {
+                    return StatusCode(401, "Đăng nhập không thành công!");
+                }
+
+                user.Password = "";
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Role, user.Role.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                var authSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(ViGoConfiguration.Secret));
+
+                // Token
+                var token = new JwtSecurityToken(
+                    issuer: ViGoConfiguration.ValidIssuer,
+                    audience: ViGoConfiguration.ValidAudience,
+                    expires: DateTime.Now.AddHours(2),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(
+                        authSigningKey, SecurityAlgorithms.HmacSha256));
+
+                var accessToken = await HttpContext.GetTokenAsync("Bearer", "access_token");
+
+                return StatusCode(200, new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.GeneratorErrorMessage());
+            }
+        }
+    }
+}
---
+++ ViGo.Services/UserServices.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViGo.Domain;
+using ViGo.Repository.Core;
+using ViGo.Services.Core;
+using ViGo.Utilities;
+
+namespace ViGo.Services
+{
+    public class UserServices : BaseServices<User>
+    {
+        public UserServices(IUnitOfWork work) : base(work)
+        {
+        }
+
+        public async Task<User> Login(string email, string password)
+        {
+            User user = null;
+
+            try
+            {
+                user = await work.Users.GetAsync(
+                    u => u.Email.ToLower().Trim()
+                    .Equals(email.ToLower().Trim())
+                    && u.Password.Equals(password.Encrypt()));
+
+                return user;
+            } catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            //User user = new User
+            //{
+            //    //Id = Guid.NewGuid(),
+            //    Name = "Admin Trần Phong",
+            //    Email = "phongntse150974@fpt.edu.vn",
+            //    Password = SecurityUtilities.Encrypt("123456789"),
+            //    Role = Domain.Enumerations.UserRole.ADMIN,
+            //    Status = Domain.Enumerations.UserStatus.ACTIVE,
+            //    IsLockedOut = false
+            //};
+
+            ////user.CreatedBy = user.Id;
+            ////user.UpdatedBy = user.Id;
+
+            //await work.Users.InsertAsync(user, true);
+            //await work.SaveChangesAsync();
+
+            IEnumerable<User> users
+                = await work.Users.GetAllAsync();
+
+            return users;
+        }
+    }
+}
