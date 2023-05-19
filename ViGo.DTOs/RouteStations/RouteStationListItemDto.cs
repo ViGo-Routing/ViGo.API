9169e79e61bd99647a2b91865ec7b9856c4a665d
Tran Phong <n.tranphongse@gmail.com>
2023-05-19 12:15:48 +0700

    DTO for Get List of Routes

---
+++ ViGo.DTOs/RouteStations/RouteStationListItemDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViGo.Domain;
+
+namespace ViGo.DTOs.RouteStations
+{
+    public class RouteStationListItemDto
+    {
+        public double Longtitude { get; set; }
+        public double Latitude { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int StationIndex { get; set; }
+
+        public RouteStationListItemDto(Station station, int stationIndex)
+        {
+            Longtitude = station.Longtitude;
+            Latitude = station.Latitude;
+            Name = station.Name;
+            Address = station.Address;
+            StationIndex = stationIndex;
+        }
+    }
+}
