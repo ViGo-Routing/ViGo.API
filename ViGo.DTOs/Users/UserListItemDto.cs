1d961c70b82d68d311096704fcf4fff2d2b0250a
Tran Phong <n.tranphongse@gmail.com>
2023-05-24 21:33:22 +0700

    Booking API - Get list of Bookings

---
+++ ViGo.DTOs/Users/UserListItemDto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViGo.Domain;
+using ViGo.Domain.Enumerations;
+
+namespace ViGo.DTOs.Users
+{
+    public class UserListItemDto
+    {
+        public Guid Id { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        //public string Password { get; set; } = null!;
+        public string? Name { get; set; }
+        public bool? Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? AvatarUrl { get; set; }
+        public UserRole Role { get; set; }
+        public UserStatus Status { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public Guid UpdatedBy { get; set; }
+        //public bool IsLockedOut { get; set; }
+        //public DateTime? LockedOutStart { get; set; }
+        //public DateTime? LockedOutEnd { get; set; }
+
+        public UserListItemDto(User user)
+        {
+            Id = user.Id;
+            Email = user.Email;
+            Phone = user.Phone;
+            Name = user.Name;
+            Gender = user.Gender;
+            DateOfBirth = user.DateOfBirth;
+            AvatarUrl = user.AvatarUrl;
+            Role = user.Role;
+            Status = user.Status;
+            CreatedTime = user.CreatedTime;
+            CreatedBy = user.CreatedBy;
+            UpdatedTime = user.UpdatedTime;
+            UpdatedBy = user.UpdatedBy;
+        }
+    }
+}
--- ViGo.Services/BookingServices.cs
+++ ViGo.Services/BookingServices.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ViGo.Domain;
+using ViGo.DTOs.Bookings;
+using ViGo.DTOs.RouteStations;
+using ViGo.DTOs.Users;
 using ViGo.Repository.Core;
 using ViGo.Services.Core;
 
@@ -13,6 +16,66 @@ namespace ViGo.Services
     {
         public BookingServices(IUnitOfWork work) : base(work)
         {
+        }
+
+        public async Task<IEnumerable<BookingListItemDto>>
+            GetBookingsAsync(Guid? userId = null)
+        {
+            IEnumerable<Booking> bookings =
+                await work.Bookings.GetAllAsync(
+                    query => query.Where(
+                        b =>
+                        (userId != null && userId.HasValue) ?
+                        b.CustomerId.Equals(userId.Value)
+                        : true));
+
+            IEnumerable<Guid> customerIds = bookings.Select(b => b.CustomerId);
+            IEnumerable<Guid> routeStationIds = bookings.Select(
+                b => b.StartRouteStationId).Concat(bookings.Select(
+                    b => b.EndRouteStationId));
+
+            IEnumerable<User> users = await work.Users
+                .GetAllAsync(query => query.Where(
+                    u => customerIds.Contains(u.Id)));
+            IEnumerable<UserListItemDto> userDtos =
+                from user in users
+                select new UserListItemDto(user);
+
+            IEnumerable<RouteStation> routeStations = await work.RouteStations
+                .GetAllAsync(query => query.Where(
+                    rs => routeStationIds.Contains(rs.Id)));
+            IEnumerable<Guid> stationIds = routeStations.Select(rs => rs.StationId);
+            IEnumerable<Station> stations = await work.Stations
+                .GetAllAsync(query => query.Where(
+                    s => stationIds.Contains(s.Id)));
+
+            IEnumerable<Guid> vehicleTypeIds = bookings.Select(b => b.VehicleTypeId);
+            IEnumerable<VehicleType> vehicleTypes = await work.VehicleTypes
+                .GetAllAsync(query => query.Where(
+                    v => vehicleTypeIds.Contains(v.Id)));
+
+            IEnumerable<BookingListItemDto> dtos =
+                from booking in bookings
+                join customer in userDtos
+                    on booking.CustomerId equals customer.Id
+                join startRouteStation in routeStations
+                    on booking.StartRouteStationId equals startRouteStation.Id
+                join startStation in stations
+                    on startRouteStation.StationId equals startStation.Id
+                join endRouteStation in routeStations
+                    on booking.EndRouteStationId equals endRouteStation.Id
+                join endStation in stations
+                    on endRouteStation.StationId equals endStation.Id
+                join vehicleType in vehicleTypes
+                    on booking.VehicleTypeId equals vehicleType.Id
+                select new BookingListItemDto(
+                    booking, customer,
+                    new RouteStationListItemDto(startRouteStation, startStation),
+                    new RouteStationListItemDto(endRouteStation, endStation),
+                    vehicleType
+                    );
+
+            return dtos;
         }
     }
 }
