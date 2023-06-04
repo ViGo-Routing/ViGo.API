2a9bc420992e0975360a2d996551e195c2651ea2
Tran Phong <n.tranphongse@gmail.com>
2023-06-04 21:24:03 +0700

    Console Program Helper

---
+++ ViGoConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using ViGo.Utilities;
+using ViGo.Utilities.Configuration;
+
+namespace ViGoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Configure();
+            Console.WriteLine("admin".Encrypt());
+        }
+
+        static void Configure()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+            IConfigurationRoot configuration = builder.Build();
+            ViGoConfiguration.Initialize(configuration);
+        }
+    }
+}
