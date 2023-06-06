f082625db4ca6118c1043e07a2f8563f4b9a3819
Tran Phong <n.tranphongse@gmail.com>
2023-06-06 20:28:12 +0700

    Firebase - Notification Utilities
    Have not called yet

--- ViGo.Domain/User.cs
+++ ViGo.Domain/User.cs
@@ -21,6 +21,7 @@ namespace ViGo.Domain
 
         public override Guid Id { get; set; }
         public string? FirebaseUid { get; set; }
+        public string? FcmToken { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Password { get; set; }
---
+++ ViGo.Utilities/FirebaseUtilities.cs
@@ -0,0 +1,90 @@
+﻿using FirebaseAdmin.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViGo.Utilities
+{
+    public static class FirebaseUtilities
+    {
+        public static async Task<string> SendNotificationToDeviceAsync(string fcmToken, 
+            string title, string content, string? imageUrl)
+        {
+            Notification notification = new Notification
+            {
+                Title = title,
+                Body = content,
+                ImageUrl = imageUrl,
+            };
+            Message message = new Message
+            {
+                Notification = notification,
+                Token = fcmToken,
+            };
+
+            string response = await FirebaseMessaging.DefaultInstance
+                .SendAsync(message);
+
+            return response;
+        }
+
+        public static async Task<IEnumerable<string>> SendNotificationToDevicesAsync
+            (IList<string> fcmTokens,
+            string title, string content, string? imageUrl)
+        {
+            Notification notification = new Notification
+            {
+                Title = title,
+                Body = content,
+                ImageUrl = imageUrl
+            };
+            MulticastMessage message = new MulticastMessage
+            {
+                Notification = notification,
+                Tokens = fcmTokens.ToList()
+            };
+
+            var response = await FirebaseMessaging.DefaultInstance
+                .SendMulticastAsync(message);
+
+            IEnumerable<string> failures = new List<string>();
+            if (response.FailureCount > 0)
+            {
+                int responseCount = response.Responses.Count;
+                for (int i = 0; i < responseCount; i++)
+                {
+                    if (!response.Responses[i].IsSuccess)
+                    {
+                        failures = failures.Append(fcmTokens[i]);
+                    }
+                }
+            }
+
+            return failures;
+        }
+
+        public static async Task<string> SendNotificationToTopicAsync(
+            string topic, string title, string content, string? imageUrl)
+        {
+            Notification notification = new Notification
+            {
+                Title = title,
+                Body = content,
+                ImageUrl = imageUrl
+            };
+
+            Message message = new Message
+            {
+                Notification = notification,
+                Topic = topic
+            };
+
+            string response = await FirebaseMessaging.DefaultInstance
+                .SendAsync(message);
+
+            return response;
+        }
+    }
+}
