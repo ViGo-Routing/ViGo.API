4ae3ead401e7d4ac1543556c27985563f1a68b55
Tran Phong <n.tranphongse@gmail.com>
2023-06-08 17:20:11 +0700

    Trip Fare Calculation - Basic
    TESTING only

---
+++ ViGo.Models/Fares/FareDiscount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViGo.Models.Fares
+{
+    public class FareDiscount
+    {
+        public short NumberOfTickets { get; set; }
+        public double Discount { get; set; }
+
+        public FareDiscount(short numberOfTickets, double discount)
+        {
+            NumberOfTickets = numberOfTickets;
+            Discount = discount;
+        }
+    }
+}
