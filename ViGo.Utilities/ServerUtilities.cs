58527adefdea7cba96108dc3f44cda457161d30a
Tran Phong <n.tranphongse@gmail.com>
2023-06-21 22:11:17 +0700

    Update for Utilities

--- ViGo.Utilities/ServerUtilities.cs
+++ ViGo.Utilities/ServerUtilities.cs
@@ -9,6 +9,13 @@ namespace ViGo.Utilities
 {
     public static class ServerUtilities
     {
+        public static string GetBaseUri(this HttpContext context)
+        {
+            string baseUri = $"{context.Request.Scheme}://" +
+                $"{context.Request.Host.Value}";
+            return baseUri;
+        }
+
         public static string GetApiBaseUrl(this HttpContext context)
         {
             string apiUrl = $"{context.Request.Scheme}://" +
