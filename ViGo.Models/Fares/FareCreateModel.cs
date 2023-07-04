79ae48453403e5dc1e8e7ebe7936f92a1250849d
Tran Phong <n.tranphongse@gmail.com>
2023-07-04 21:22:05 +0700

    Create FareCreateModel.cs

---
+++ ViGo.Models/Fares/FareCreateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViGo.Models.Fares
+{
+    public class FareCreateModel
+    {
+        public Guid VehicleTypeId { get; set; }
+        public double BaseDistance { get; set; }
+        public double BasePrice { get; set; }
+    }
+}
