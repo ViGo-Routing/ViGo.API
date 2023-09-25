0ae31cef227a72ac0559e9cf00f5b3ec1d95acc5
Tran Phong <n.tranphongse@gmail.com>
2023-09-25 16:51:55 +0700

    Update for Authorization

--- ViGo.API/Controllers/SettingController.cs
+++ ViGo.API/Controllers/SettingController.cs
@@ -34,7 +34,7 @@ namespace ViGo.API.Controllers
         /// <response code="200">Get List of settings successfully</response>
         /// <response code="500">Server error</response>
         [HttpGet]
-        [Authorize(Roles = "ADMIN,CUSTOMER")]
+        [Authorize]
         [ProducesResponseType(typeof(IEnumerable<SettingViewModel>), 200)]
         [ProducesResponseType(403)]
         [ProducesResponseType(401)]
