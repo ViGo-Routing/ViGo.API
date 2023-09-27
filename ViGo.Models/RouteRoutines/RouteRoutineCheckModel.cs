97d4070a1020313a392cb1a67ad86f88826db09c
Tran Phong <n.tranphongse@gmail.com>
2023-09-27 16:19:31 +0700

    Update for routines check

--- ViGo.Models/RouteRoutines/RouteRoutineCheckModel.cs
+++ ViGo.Models/RouteRoutines/RouteRoutineCheckModel.cs
@@ -1,11 +1,28 @@
-﻿namespace ViGo.Models.RouteRoutines
+﻿using ViGo.Models.GoogleMaps;
+
+namespace ViGo.Models.RouteRoutines
 {
     public class RouteRoutineCheckModel
     {
         public Guid RouteId { get; set; }
+        public GoogleMapPoint? StartPoint { get; set; }
+        public GoogleMapPoint? EndPoint { get; set; }
         public IList<RouteRoutineListItemModel> RouteRoutines { get; set; }
             = new List<RouteRoutineListItemModel>();
         //public RouteRoutineAction Action { get; set; } = RouteRoutineAction.CREATE;
+    }
+
+    public class RoundRouteRoutineCheckModel
+    {
+        public Guid RouteId { get; set; }
+        public GoogleMapPoint? StartPoint { get; set; }
+        public GoogleMapPoint? EndPoint { get; set; }
+
+        public IList<RouteRoutineListItemModel> RouteRoutines { get; set; }
+            = new List<RouteRoutineListItemModel>();
+
+        public IList<RouteRoutineListItemModel> RoundRouteRoutines { get; set; }
+            = new List<RouteRoutineListItemModel>();
     }
 
     public enum RouteRoutineAction
